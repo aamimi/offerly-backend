e6c67ccda49e1046398d3236e13987c63169a401
aamimi <amimi.achref@gmail.com>
2025-02-08 02:09:22 -0500

    feat: update default category SVG and implement temporary thumbnail retrieval for products

--- app/Http/Resources/v1/Product/IndexResource.php
+++ app/Http/Resources/v1/Product/IndexResource.php
@@ -26,8 +26,31 @@ final class IndexResource extends JsonResource
             'summary' => $this->summary,
             'price' => $this->price,
             'discount_price' => $this->discount_price,
-            'thumbnail' => 'https://placehold.co/150x150',
+            'thumbnail' => $this->getThumbnail(),
             'rating' => $this->rating,
         ];
+    }
+
+    /**
+     * Get a random thumbnail image.
+     * temporary solution for the demo.
+     * TODO: Implement a proper solution.
+     */
+    private function getThumbnail(): string
+    {
+        $images = [
+            'https://picsum.photos/200/200?random=1',
+            'https://picsum.photos/200/200?random=2',
+            'https://picsum.photos/200/200?random=3',
+            'https://picsum.photos/200/200?random=4',
+            'https://picsum.photos/200/200?random=5',
+            'https://picsum.photos/200/200?random=6',
+            'https://picsum.photos/200/200?random=7',
+            'https://picsum.photos/200/200?random=8',
+            'https://picsum.photos/200/200?random=9',
+            'https://picsum.photos/200/200?random=10',
+        ];
+
+        return $images[random_int(0, 9)];
     }
 }
